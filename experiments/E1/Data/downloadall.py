470cec257e5f595bee2a6edc20421d232d41c5b0
Aurelien Fermo <aurelien.fermo@ens.psl.eu>
2020-05-26 18:44:52 +1000

    last update

--- experiments/E1/Data/downloadall.py
+++ experiments/E1/Data/downloadall.py
@@ -1,5 +1,10 @@
 #export GOOGLE_APPLICATION_CREDENTIALS="~/path/to/gae-json-token"
 
+import os
+import pandas as pd
+import numpy as np
+from io import StringIO
+
 # Imports the Google Cloud client library
 from google.cloud import datastore
 
@@ -12,7 +17,14 @@ datastore_client = datastore.Client()
 # make query
 query = datastore_client.query(kind='DataObject')
 results = list(query.fetch())
-print(json.dumps(results, indent=4, sort_keys=True, default=str))
+df = pd.read_json(json.dumps(results, indent=4, sort_keys=True, default=str))
 
-# system call: pass it to a text file
-# python downloadall.py > mydata.txt
+data = pd.DataFrame()
+for idx,row in enumerate(df.iterrows()):
+    row_data = StringIO(row[1].content)
+    dataframe = pd.read_csv(row_data, sep=',')
+    dataframe.insert(0,"subject_id",np.repeat(idx+1,dataframe.shape[0]))
+    data = pd.concat([data,dataframe],ignore_index=True)
+    
+cd = os.getcwd()
+data.to_csv(r"{}/data.csv".format(cd),index=False)
